36be60040054d5cc991e34e5240d2ba27e9cfaac
EASV70R <75667950+EASV70R@users.noreply.github.com>
2023-10-23 20:42:40 +0200

    Update logs.php

--- views/admin/logs.php
+++ views/admin/logs.php
@@ -5,72 +5,6 @@ Util::Header();
 
 <main class="container-fluid mt-5">
     <div class="testcontainer">
-        <div class="modal fade" id="editmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel"> User Data </h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form method="POST">
-                            <input type="hidden" name="uid" id="uid">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Username" name="mUsername"
-                                    id="mUsername" minlength="3" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Password" name="mPassword"
-                                    id="mPassword" minlength="4">
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="email" name="mEmail" id="mEmail"
-                                    required>
-                            </div>
-                            <div class="form-group">
-                                <select class="form-select" aria-label="Default select example" name="mRole">
-                                    <option selected>Role Selection</option>
-                                    <option value="0">Customer</option>
-                                    <option value="1">Admin</option>
-                                </select>
-                                <!-- <input type="text" class="form-control" placeholder="role" name="mRole" id="mRole" required> -->
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button class="btn btn-primary btn-block" name="edit" id="edit" type="edit"
-                                    value="edit">
-                                    Edit
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade" id="deletemodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel"> Delete User Data </h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form method="POST">
-                            <input type="hidden" name="uid" id="deleteid">
-
-                            <h4>Are you sure about this?</h4>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-                                <button class="btn btn-primary btn-block" name="delete" id="delete" type="delete"
-                                    value="delete">
-                                    Yes
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="row justify-content-center">
             <div class="col-12 mt-3 mb-2">
                 <?php if (isset($response)) : ?>
@@ -107,53 +41,20 @@ Util::Header();
                                 <button class="btn btn-danger deletebtn" data-id="2" data-toggle="modal">Delete</button>
                             </td>
                         </tr>
+                        <tr>
+                            <td><?= 'test' ?></td>
+                            <td><?= 'test' ?></td>
+                            <td><?= 'test' ?></td>
+                            <td>
+                                <button class="btn btn-primary editbtn" data-id="1" data-toggle="modal">Edit</button>
+                                <button class="btn btn-danger deletebtn" data-id="2" data-toggle="modal">Delete</button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
-
             </div>
         </div>
     </div>
 </main>
 
 <?php Util::Footer(); ?>
-
-<script>
-$(document).ready(function() {
-
-    $('.editbtn').on('click', function() {
-
-        $('#editmodal').modal('show');
-
-        $tr = $(this).closest('tr');
-
-        var data = $tr.children("td").map(function() {
-            return $(this).text();
-        }).get();
-
-        console.log(data);
-
-        $('#uid').val(data[0]);
-        $('#mUsername').val(data[1]);
-        $('#mEmail').val(data[2]);
-        $('#mRole').val(data[3]);
-    });
-});
-
-$(document).ready(function() {
-
-    $('.deletebtn').on('click', function() {
-
-        $('#deletemodal').modal('show');
-
-        $tr = $(this).closest('tr');
-
-        var data = $tr.children("td").map(function() {
-            return $(this).text();
-        }).get();
-
-        console.log(data);
-
-        $('#deleteid').val(data[0]);
-    });
-});
-</script>
